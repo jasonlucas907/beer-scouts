2b43299ad0b840527eef4f5f4a1328cf2ab2fb0e
Jason Lucas <jlucas5280@gmail.com>
2018-01-12 16:10:49 -0700

    clouds

--- src/Clouds/Clouds.js
+++ src/Clouds/Clouds.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import cloud1 from '../assets/cloud1.png';
 import cloud2 from '../assets/cloud2.png';
+import cloud3 from '../assets/cloud3.png';
+import cloud4 from '../assets/cloud4.png';
 import './Clouds.css';
 
 
@@ -12,6 +14,8 @@ const Clouds = ({ }) => {
 
       <img className='cloud1' src={cloud1} alt='cloud illustration'/>
       <img className='cloud2' src={cloud2} alt='cloud illustration'/>
+      <img className='cloud3' src={cloud3} alt='cloud illustration'/>
+      <img className='cloud4' src={cloud4} alt='cloud illustration'/>
 
     </div>
   )
