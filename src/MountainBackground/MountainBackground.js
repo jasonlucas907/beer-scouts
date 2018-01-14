9beacb04e01c27818aae9672ab3f2b7a69d986a2
Jason Lucas <jlucas5280@gmail.com>
2018-01-14 08:09:41 -0700

    flying bird

--- src/MountainBackground/MountainBackground.js
+++ src/MountainBackground/MountainBackground.js
@@ -4,6 +4,7 @@ import mountain1 from '../assets/mtn1.png';
 import mountain2 from '../assets/mtn2.png';
 import mountain3 from '../assets/mtn3.png';
 import mountain4 from '../assets/mtn4.png';
+import birds1 from '../assets/birds2.gif';
 
 const MountainBackground = ({ }) => {
 
@@ -14,6 +15,7 @@ const MountainBackground = ({ }) => {
       <img className='mountain1' src={ mountain1 } alt='outline image of mountain landscape'/>
       <img className='mountain2' src={ mountain2 } alt='outline image of mountain landscape'/>
       <img className='mountain3' src={ mountain3 } alt='outline image of mountain landscape'/>
+      <img className='home-page-birds' src={ birds1 } alt='flock of birds'/>
       <img className='mountain4' src={ mountain4 } alt='outline image of mountain landscape'/>
 
     </div>
